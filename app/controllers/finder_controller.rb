b2f4401a65f4a57f38fa6297d4a354ad68770e7d
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-08-09 23:12:28 +0300

    Finish the missing_email and alphabetized actions

--- app/controllers/finder_controller.rb
+++ app/controllers/finder_controller.rb
@@ -1,5 +1,18 @@
 class FinderController < ApplicationController
   def index
     @customers = Customer.all
+    @page_title = 'All Customers'
+  end
+
+  def alphabetized
+    @customers = Customer.order(:full_name)
+    @page_title = 'Customers in Alphabetical Order'
+    render :index
+  end
+
+  def missing_email
+    @customers = Customer.where(email_address: '')
+    @page_title = 'Customers missing email address'
+    render :index
   end
 end
