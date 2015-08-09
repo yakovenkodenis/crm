20d0825becdd287061c04539ea9bf7d752126450
Denis Yakovenko <yakovenko.denis.a@gmail.com>
2015-08-09 22:14:08 +0300

    Create customer partial and Finder controller

---
+++ app/controllers/finder_controller.rb
@@ -0,0 +1,5 @@
+class FinderController < ApplicationController
+  def index
+    @customers = Customer.all
+  end
+end
---
+++ spec/controllers/finder_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe FinderController, type: :controller do
+
+end
